3ff52e34844fe55033903edbf14186b083a2a324
Pedro Pais <pgpaisdev@gmail.com>
2021-12-15 08:29:01 +0000

    Reorganized code. added context to some decisions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     #region Input Fields
-        public Vector2 movInput;
+    public Vector2 movInput;
 
-        public Vector2 aimInput;
+    public Vector2 aimInput;
     #endregion
 
     [SerializeField] float movSpeed = 5f;
@@ -22,43 +22,35 @@ public class PlayerController : MonoBehaviour
     [SerializeField] LayerMask interactLayer;
 
     #region References
-        [Header("References")]
-        [SerializeField] Transform cam;
-        Rigidbody rb;
+    [Header("References")]
+    [SerializeField] Transform cam;
+    Rigidbody rb;
     #endregion
 
     float mouseRot;
 
+    // Awake happens before Start. Use it to initialize references
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
         mouseRot = cam.rotation.eulerAngles.x;
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         DoAim();
-    }
-
-    
-    void FixedUpdate()
-    {
-        DoMovement();
-    }
-
-    private void DoMovement()
-    {
-        Vector3 forward = transform.forward * movInput.y;
-        Vector3 right = transform.right * movInput.x;
-
-        rb.velocity = (forward + right) * movSpeed + new Vector3(0, rb.velocity.y);
     }
 
     private void DoAim()
     {
         // Rotate Body
-        transform.Rotate(new Vector3(0, aimInput.x));
+        transform.Rotate(Vector3.up * aimInput.x);
 
         // Rotate Camera
 
@@ -73,13 +65,30 @@ public class PlayerController : MonoBehaviour
         cam.rotation = Quaternion.Euler(newRot);
     }
 
-    public void Interact(){
+    void FixedUpdate()
+    {
+        DoMovement();
+    }
+
+    private void DoMovement()
+    {
+        Vector3 forward = transform.forward * movInput.y;
+        Vector3 right = transform.right * movInput.x;
+
+        // Since this happens in the FixedUpdate function, you don't need to multiply by Time.DeltaTime
+        // If you want to, however, you can use Time.fixedDeltaTime (althought it is a constant)
+        rb.velocity = (forward + right) * movSpeed + new Vector3(0, rb.velocity.y);
+    }
+
+    public void Interact()
+    {
         Debug.DrawRay(cam.position, cam.forward * interactRange);
-        
+
         RaycastHit hit;
         // Fires a ray from the camera to cam.forward + interactRange
         // only hitting objects on the interactLayer (Button, in this case)
-        if(Physics.Raycast(cam.position, cam.forward, out hit, interactRange, interactLayer)){
+        if (Physics.Raycast(cam.position, cam.forward, out hit, interactRange, interactLayer))
+        {
             hit.collider.GetComponent<Button>().Interact();
         }
     }

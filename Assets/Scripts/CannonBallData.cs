57ce44ca1cbb1c60c1bc7bf53188f058f94e2daf
Pedro Pais <pgpaisdev@gmail.com>
2021-12-15 09:27:50 +0000

    stripped main down

--- Assets/Scripts/CannonBallData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CannonBallData", menuName = "CannonBallData", order = 0)]
-public class CannonBallData : ScriptableObject {
-    public float launchForce = 10f;
-    public float timeToDie = 2f;
-}
